36527bdb1af816fb576ae4dfd64141d78584f9df
kali.lxcyfxr <kali@lxcyfxr.com>
2026-02-11 14:31:48 -0500

    droopescan fork added python 3.12 support

--- tools.sh
+++ tools.sh
@@ -18,6 +18,7 @@ REPOS=(
   "https://github.com/ajnik/joomla-bruteforce.git"
   "https://github.com/drego85/JoomlaScan.git"
   "https://github.com/enjoiz/XXEinjector.git"
+  "https://github.com/sboden/droopescan.git" #droopescan fork with python 3.12 support
 )
 
 echo "Cloning all repos into $INSTALL_DIR..."
